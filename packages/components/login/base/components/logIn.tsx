629d4d107c9afeee1f057a0644bfce6347dd881f
yuzhench2004 <yuzhench2004@gmail.com>
2025-01-23 14:50:13 +0000

    update the design login

--- packages/components/login/base/components/logIn.tsx
+++ packages/components/login/base/components/logIn.tsx
@@ -12,65 +12,69 @@ import './login.css'
 
 export const LogIn = (): any => {//any = unknow type data /anything
 
-    return (
-        <Box className='box-base'
-            component="form"
-            noValidate
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              width: '100%',
-              gap: 2,
-            }}
-          >
-            <FormControl>
-              <FormLabel htmlFor="email">Email</FormLabel>
-              <TextField
-                id="email"
-                type="email"
-                name="email"
-                placeholder="your@email.com"
-                autoComplete="email"
-                autoFocus
-                required
-                fullWidth
-                variant="outlined"
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel htmlFor="password">Password</FormLabel>
-              <TextField
-                name="password"
-                placeholder="••••••"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                autoFocus
-                required
-                fullWidth
-                variant="outlined"
-              />
-            </FormControl>
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-            >
-              Sign in
-            </Button>
-            <Link
-              component="button"
-              type="button"
-              variant="body2"
-              sx={{ alignSelf: 'center' }}
-            >
-              Forgot your password?
-            </Link>
-          </Box>
-    )
+  return (
+    <>
+    <div className='div-box'>
+      <Box className='box-base'
+        component="form"
+        noValidate
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          width: '75%',
+          gap: 2,
+        }}
+      >
+        <FormControl>
+          <FormLabel htmlFor="email">Email</FormLabel>
+          <TextField
+            id="email"
+            type="email"
+            name="email"
+            placeholder="Hello@email.com"
+            autoComplete="email"
+            autoFocus
+            required
+            fullWidth
+            variant="outlined"
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel htmlFor="password">Password</FormLabel>
+          <TextField
+            name="password"
+            placeholder=""
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            autoFocus
+            required
+            fullWidth
+            variant="outlined"
+          />
+        </FormControl>
+        <FormControlLabel
+          control={<Checkbox value="remember" color="primary" />}
+          label="Remember me"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+        >
+          Sign in
+        </Button>
+        <Link
+          component="button"
+          type="button"
+          variant="body2"
+          sx={{ alignSelf: 'center' }}
+        >
+          Forgot your password?
+        </Link>
+      </Box>
+      </div>
+    </>
+  )
 }
 //              onClick={}
--- packages/components/login/base/components/login-header-form.tsx
+++ packages/components/login/base/components/login-header-form.tsx
@@ -5,10 +5,12 @@ export const LoginHeader = (): any => {//any = unknow type data /anything
 
     return (
         <>
+        <div className='centralize-header'>
             <div className="base_header">
                 <div> <p> <strong className="titled"> Meow Tea</strong> 
                 <img className="logo" src={"/packages/images/matcha_boba_tea.png"} alt="logo matcha boba tea smiling"/></p>
                 </div>
+            </div>
             </div>
         </>
     )
